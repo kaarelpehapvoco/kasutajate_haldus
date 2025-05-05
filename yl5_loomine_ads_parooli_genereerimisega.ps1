a0050680bbc09168a824d12d03595e659da5869c
Kaarel Pehap <kaarel.pehap@voco.ee>
2025-05-05 19:35:12 +0300

    1. yl5 commit

--- yl5_loomine_ads_parooli_genereerimisega.ps1
+++ yl5_loomine_ads_parooli_genereerimisega.ps1
@@ -14,4 +14,42 @@ $PassComplexCheck=$True
 }
 } While ($PassComplexCheck -eq $false)
 return $newPassword
+
+
+
+
+
+}
+  Write-Error "Viga AD päringul: $_"
+    exit 1
+}
+
+if ($on_olemas) {
+    Write-Host "Kasutaja '$taisnimi' (kasutajanimi: $kasutajanimi) on juba olemas AD-s."
+    exit 0
+}
+
+# Küsib algse parooli
+$parool = GenerateStrongPassword()
+
+# AD kasutaja loomine
+$ou_tee = 'OU=Kasutajad,DC=pehap,DC=local'  # Muuda
+
+try {
+    New-ADUser \
+        -Name               $taisnimi \
+        -GivenName          $eesnimi \
+        -Surname            $perenimi \
+        -SamAccountName     $kasutajanimi \
+        -UserPrincipalName  $upn \
+        -AccountPassword    $parool \
+        -Enabled            $true \
+        -Path               $ou_tee \
+        -ChangePasswordAtLogon $true \
+        -ErrorAction        Stop
+
+    Write-Host "Kasutaja '$kasutajanimi' loodud edukalt AD-s." -ForegroundColor Green
+} catch {
+    Write-Error "Viga kasutaja loomisel: $_"
+    exit 1
 }
